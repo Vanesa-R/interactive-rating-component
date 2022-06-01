2a4ac3ba6081934918f0575ccfd1ad6a52641245
Vanesa <uve.rdguez@gmail.com>
2022-06-01 21:08:35 +0200

    index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "../src/lang/i18n-setting";
+import "./lang/i18n-setting";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
